413a762f13055a88908a87bdf3f585d9c0cc6d28
Hadrien Belleville <hadrien.belleville22@gmail.com>
2024-07-02 00:12:13 +0200

    :sparkles: Ground setup for implementing 3D

--- renderer.hpp
+++ renderer.hpp
@@ -26,6 +26,11 @@ namespace vraus_VulkanEngine {
 			return commandBuffers[currentImageIndex]; 
 		}
 
+		int getFrameIndex() const {
+			assert(isFrameStarted && "Cannot get frame index when frame not in progress.");
+			return currentFrameIndex;
+		}
+
 		// We want the application to main control over every steps of drawing a frame
 		// so that down the line we can easily integrate multiple render passes
 		// Will be helpfull for things like reflections, shadows, raytracing, postprocessing effects
@@ -44,7 +49,9 @@ namespace vraus_VulkanEngine {
 		Device& device;
 		std::unique_ptr<SwapChain> swapChain;
 		std::vector<VkCommandBuffer> commandBuffers;
+
 		uint32_t currentImageIndex;
+		int currentFrameIndex{ 0 }; // Keep track of a frameIndex : [0, Max_Frames_In_Flight] not tight to the image index.
 		bool isFrameStarted{ false };
 	};
 }
